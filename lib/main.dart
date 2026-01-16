38c4237eec81425a241262c7404230d80484c425
anomali01 <agus8u7@gmail.com>
2026-01-16 23:55:45 +0700

    tambah fitur debug untuk proses izin pulang dan konversi ke Firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:perizinan_santri/Views/HomeKeamanan.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize date formatting
+  await initializeDateFormatting();
 
   try {
     await Firebase.initializeApp(
