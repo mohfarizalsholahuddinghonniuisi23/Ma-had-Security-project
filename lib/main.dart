f0157a36c607c5f1cb152a9c1d51a595fcc9ef8d
anomali01 <agus8u7@gmail.com>
2026-01-16 21:31:59 +0700

    bersihkan impor yang tidak digunakan dan perbaiki format kode di main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:perizinan_santri/Views/HomeKeamanan.dart';
 import 'firebase_options.dart';
-import 'screens/test_firestore_screen.dart';
-import 'Views/HomeKeamanan.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key?  key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
